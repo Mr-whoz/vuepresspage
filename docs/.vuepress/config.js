1b68f0f291237ec36bed2366b3684ba434de966b
Mr-whoz <1124333578@qq.com>
2025-08-10 21:27:39 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+
+  title: '你好， VuePress ！',
+  description: 'My first VuePress Site',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: ['/', '/get-started'],
+  }),
+
+  bundler: viteBundler(),
+})
+
+module.exports = {
+  base: '/', // 根据实际情况修改
+  // 其他配置...
+}
